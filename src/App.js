1399f68ab0125753395484eced91d64b6bb7d3a0
sivakumar_b <sivakumar_b@solartis.com>
2023-09-12 18:00:28 +0530

    adding link to home, shop and contact page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import HomePage from './Pages/Homepage/HomePage.Component';
 import { Routes, Route } from 'react-router-dom';
 import Shoppage from './Pages/Shoppage/Shoppage.component';
+import Header from './Components/header/Header.component';
 
 
 
 function App() {
   return (
     <div >
+      <Header/>
         <Routes>
           <Route exact path='/' Component={HomePage} />
           <Route path='/shop' Component={Shoppage} />
